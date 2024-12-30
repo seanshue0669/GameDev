863f192d9a201020c5a8bfc268006ed5642d8179
salmon <duckduckcute74@gmail.com>
2024-12-31 07:49:55 +0800

    ui, interaction
    add basic ui to show the interaction,  and you can press E to interact with four kinds of minigame table(i add the box collider in these)

---
+++ Assets/ScenesAsset/Script/Testing/Interaction.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Interaction : MonoBehaviour
+{
+    public float interactDistance = 3f; // 交互距离
+    public LayerMask Layer; // 设置箱子所在的 Layer
+    public TextMeshProUGUI interactText; // UI 文本，拖拽到 Inspector 中
+    public Scene.SceneLoader sceneLoader;
+
+    private GameObject currentBox = null; // 当前目标箱子
+
+    void Update()
+    {
+        RaycastHit hit;
+        // 从摄像机的位置向前发射射线
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, Layer))
+        {
+            string tag = hit.collider.tag;
+
+            // 如果射线命中并且命中的是箱子
+            if (tag == "Dicegame")
+            {
+                currentBox = hit.collider.gameObject;
+                // 显示交互提示
+                if (interactText != null)
+                {
+                    interactText.gameObject.SetActive(true);
+                    interactText.text = "Press E to play DiceGame";
+                }
+
+                // 检测是否按下 E 键
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //sceneLoader.LoadScene("DiceScecne");
+                    Debug.Log("DiceScecne");
+                }
+                return; // 如果有命中，结束 Update
+            }
+            else if (tag == "Blackjack")
+            {
+                currentBox = hit.collider.gameObject;
+                // 显示交互提示
+                if (interactText != null)
+                {
+                    interactText.gameObject.SetActive(true);
+                    interactText.text = "Press E to play Blackjack";
+                }
+
+                // 检测是否按下 E 键
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //sceneLoader.LoadScene("DiceScecne");
+                    Debug.Log("Black");
+                }
+                return; // 如果有命中，结束 Update
+            }
+            else if (tag == "RouletteGame")
+            {
+                currentBox = hit.collider.gameObject;
+                // 显示交互提示
+                if (interactText != null)
+                {
+                    interactText.gameObject.SetActive(true);
+                    interactText.text = "Press E to play RouletteGame";
+                }
+
+                // 检测是否按下 E 键
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //sceneLoader.LoadScene("DiceScecne");
+                    Debug.Log("RouletteGame");
+                }
+                return; // 如果有命中，结束 Update
+            }
+            else if (tag == "SlotGame")
+            {
+                currentBox = hit.collider.gameObject;
+                // 显示交互提示
+                if (interactText != null)
+                {
+                    interactText.gameObject.SetActive(true);
+                    interactText.text = "Press E to play SlotGame";
+                }
+
+                // 检测是否按下 E 键
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    //sceneLoader.LoadScene("DiceScecne");
+                    Debug.Log("SlotGame");
+                }
+                return; // 如果有命中，结束 Update
+            }
+        }
+
+        // 如果没有命中箱子，隐藏交互提示
+        currentBox = null;
+        if (interactText != null)
+        {
+            interactText.gameObject.SetActive(false);
+        }
+    }
+}
