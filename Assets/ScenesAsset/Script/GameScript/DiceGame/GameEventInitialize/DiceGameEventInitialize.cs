260686f27165f0ed284762225360a1d50d34cce4
seanshue <seanshue0423@gmail.com>
2024-12-31 14:30:49 +0800

    Addding script
    Adding Event Initializer

---
+++ Assets/ScenesAsset/Script/GameScript/DiceGame/GameEventInitialize/DiceGameEventInitialize.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DiceGameEventInitiler : MonoBehaviour
+{
+    [SerializeField]
+    //Add some stuff here
+    void Awake()
+    {
+        //Call the singleton instance init function
+    }
+}
