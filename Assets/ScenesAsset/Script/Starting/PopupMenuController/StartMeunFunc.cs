dbacfb3b2ad8482bb977c8dfabf5680216ee06af
seanshue <seanshue0423@gmail.com>
2025-01-02 12:54:40 +0800

    Adding settings PLane
    Addngg Graphic settings

---
+++ Assets/ScenesAsset/Script/Starting/PopupMenuController/StartMeunFunc.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class StartMeunFunc:MonoBehaviour
+{
+    [SerializeField]
+    public GameObject settingsUI;
+    public GameObject CreaterUI;
+
+    public void Start()
+    {
+        //settingsUI = GameObject.Find("Settings");
+        //CreaterUI = GameObject.Find("Creater");
+    }
+    public void SettingsUIController(bool p_open)
+    {
+        Debug.Log("Show");
+        if(p_open)
+            settingsUI.SetActive(true);
+        else
+            settingsUI.SetActive(false);
+    }
+    public void CreaterUIController(bool p_open)
+    {
+        if(p_open)
+            CreaterUI.SetActive(true);
+        else
+            CreaterUI.SetActive(false);
+    }
+}
